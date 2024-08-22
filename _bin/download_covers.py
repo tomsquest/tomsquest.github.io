aed70768b392ca587f8887953a321ea5c8c52264
Thomas Queste <tom@tomsquest.com>
2024-08-22 08:29:55 +0200

    feat(bookshelf): save book cover in books.json and add som error handling

--- _bin/download_covers.py
+++ _bin/download_covers.py
@@ -8,6 +8,8 @@ from duckduckgo_search import DDGS
 from io import BytesIO
 from pathlib import Path
 import json
+import time
+import random
 
 
 def search_images(term: str, max_images=1) -> list[str]:
@@ -22,7 +24,7 @@ def search_images(term: str, max_images=1) -> list[str]:
     return image_urls
 
 
-def get_first_valid_image(urls: list[str]) -> ImageFile:
+def get_first_valid_image(urls: list[str]) -> Union[ImageFile, None]:
     for url in urls:
         response = requests.get(url)
         if response.status_code == 200:
@@ -31,11 +33,20 @@ def get_first_valid_image(urls: list[str]) -> ImageFile:
                 return img
             except UnidentifiedImageError:
                 pass
+    return None
 
 
-def save_image(image: ImageFile, save_directory: Path, name: str) -> Path:
+def save_image(image: ImageFile, save_directory: Path, name: str) -> Union[Path, None]:
     dest_file = save_directory / f"{name}.jpg"
-    image.save(dest_file)
+    try:
+        image.save(dest_file)
+    except OSError:
+        dest_file = save_directory / f"{name}.png"
+        try:
+            image.save(dest_file)
+        except OSError:
+            print(f"Could not save image '{name}'")
+            return None
     return dest_file
 
 
@@ -61,29 +72,50 @@ def get_volume(book) -> str:
 
 
 if __name__ == "__main__":
-    dest_folder = Path("assets/books")
-    dest_folder.mkdir(exist_ok=True)
+    books_file = "_data/books.json"
 
-    # open _data/books.json and for each book, download the cover
-    count = 0
-    with open("_data/books.json") as f:
+    assets_folder = Path("assets/books")
+    assets_folder.mkdir(exist_ok=True)
+
+    with open(books_file, "r") as f:
         books = json.load(f)
-        for book in books:
-            # Skip the book if it already has a cover
-            if book.get("cover"):
-                continue
 
-            search_query = make_search_query(book)
-            print(f"Searching for '{search_query}")
+    count = 0
+    for book in books:
+        # Skip the book if it already has a cover
+        if book.get("cover"):
+            continue
 
-            urls = search_images(search_query, max_images=10)
-            image = get_first_valid_image(urls)
-            filename = slug.slug(f"{book['title']} {get_volume(book)}")
+        search_query = make_search_query(book)
+        print(f"Searching for '{search_query}")
 
-            dest_file = save_image(image, dest_folder, filename)
-            print(f"Saved cover to: {dest_file}")
+        urls = search_images(search_query, max_images=10)
+        image = get_first_valid_image(urls)
+        if not image:
+            print(f"No valid image for book '{book['title']}'. Continuing")
+            continue
 
-            count += 1
-            if count > 5:
-                print("Early stop")
-                exit(0)
+        # Store the image
+        filename = slug.slug(f"{book['title']} {get_volume(book)}")
+        dest_file = save_image(image, assets_folder, filename)
+        if not dest_file:
+            print(f"Could not save image for book '{book['title']}'. Continuing")
+            continue
+        print(f"Saved cover to: {dest_file}")
+
+        # Add the cover path to the book
+        book["cover"] = f"/{dest_file}"
+
+        # Wait time between searches to avoid rate limiting
+        if count != len(books) - 1:
+            print("Waiting a little bit before the next search...")
+            time.sleep(1 + 2 * random.random())
+
+        # Early stopping
+        count += 1
+        if count > 5:
+            break
+
+        # Persist the updated books
+        with open(books_file, "w") as f:
+            json.dump(books, f, indent=2, sort_keys=False, ensure_ascii=False)
