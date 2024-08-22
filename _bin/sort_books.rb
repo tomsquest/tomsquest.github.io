d6475fd9ef52c2e6a4188f5d0053ca01a83e1099
Thomas Queste <tom@tomsquest.com>
2024-08-22 07:30:52 +0200

    refactor: reorg sort_books.rb

--- _bin/sort_books.rb
+++ _bin/sort_books.rb
@@ -6,21 +6,13 @@ file_path = '_data/books.json'
 books = JSON.parse(File.read(file_path))
 
 sorted_books = books.sort_by do |book|
-  date = Date.strptime(book['date'] || '1970-01', '%Y-%m')
-
-  volume = book['volume'] || '1'
-
+  date = Date.strptime(book['date'] || '1970-01', '%Y-%m').to_time.to_i
   # Group by series instead of title
   title = book['series'] || book['title'] || ''
+  volume = (book['volume'] || '1').to_i
 
-  [
-    -date.to_time.to_i,
-    title,
-    -volume.to_i,
-  ]
+  [-date, title, -volume]
 end
-
-puts(sorted_books)
 
 # Write the sorted books back to the file
 File.write(file_path, JSON.pretty_generate(sorted_books))
